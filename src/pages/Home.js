29e557ebe7aa1172c25216ea7cfdaa35aff35588
Duke-Skygawker <82309145+Duke-Skygawker@users.noreply.github.com>
2023-03-19 22:03:18 +0100

    basic grid in home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div>
-      <h1>HOME</h1>;
+    <div class="container text-center">
+      <div class="row">
+        <div class="col bg-primary">1 of 3</div>
+        <div class="col-9 bg-success">
+          <h1>HOME</h1>
+        </div>
+        <div class="col bg-primary">3 of 3</div>
+      </div>
     </div>
   );
 };
