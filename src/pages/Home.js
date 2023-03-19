dcaf481deb4c956d26420f793c7be31445f21aac
Duke-Skygawker <82309145+Duke-Skygawker@users.noreply.github.com>
2023-03-19 22:22:05 +0100

    basic home done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,133 @@ const Home = () => {
   return (
     <div class="container text-center">
       <div class="row">
-        <div class="col bg-primary">1 of 3</div>
-        <div class="col-9 bg-success">
-          <h1>HOME</h1>
+        <div class="col bg-primary">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu
+            enim mauris. Quisque a pharetra justo, eget condimentum justo.
+            Praesent nisi est, ultrices eget nisl sit amet, tristique fringilla
+            ipsum. Morbi laoreet fringilla purus, ut ultricies nisl elementum
+            pulvinar. Cras eget enim mollis, convallis quam eu, feugiat nibh.
+            Phasellus at risus quis neque egestas feugiat. Aliquam malesuada,
+            massa non rhoncus euismod, ligula velit dignissim leo, vitae
+            facilisis ligula odio vitae sem. Quisque quis nulla non elit
+            volutpat cursus. Nullam tincidunt nunc a iaculis gravida. Praesent
+            ornare libero in vehicula malesuada. Cras a massa laoreet, gravida
+            quam id, condimentum lorem. Proin ornare purus quam, in rutrum
+            turpis sollicitudin non. Etiam non libero velit. Cras pretium, sem
+            eu tincidunt efficitur, ex eros pellentesque augue, eget porta
+            turpis libero id augue. Quisque id molestie felis. Aenean ultricies
+            pharetra rutrum. Etiam tempus augue tortor, ut rhoncus urna
+            malesuada sed. Integer aliquam molestie lacus quis lacinia. Sed
+            rutrum luctus risus ut molestie. Pellentesque finibus efficitur
+            lorem, ac rhoncus justo cursus a. Suspendisse elit ex, aliquet nec
+            viverra ut, pretium egestas nisi. Vivamus lorem augue, elementum ut
+            blandit ut, fermentum at justo. Fusce eleifend vulputate justo vitae
+            tristique. Maecenas maximus sed urna a placerat. Ut lacinia nulla
+            nec velit venenatis vehicula. Sed tempor ex id magna molestie
+            luctus. Pellentesque iaculis lectus vitae elit facilisis, vel
+            sodales ipsum mattis. Nam ex est, aliquam et congue tincidunt,
+            rutrum vulputate erat. Donec cursus cursus dolor sit amet laoreet.
+            Suspendisse eu feugiat diam. Maecenas scelerisque orci nibh, ut
+            finibus mauris ultrices vitae. Suspendisse vel auctor lectus.
+            Curabitur massa mi, aliquet et vulputate eu, consectetur vitae arcu.
+            Praesent in eros gravida, faucibus velit ut, elementum libero. Nulla
+            facilisi. Pellentesque quis viverra libero. Nullam ultrices luctus
+            lacus non interdum. Aliquam lacinia lorem eu nulla tincidunt, sit
+            amet tincidunt sem laoreet. Nulla facilisi. Duis elit diam, feugiat
+            ac ante et, molestie placerat est. Nullam tincidunt et tortor quis
+            egestas. Nunc ullamcorper mauris dui. Proin eu egestas libero.
+            Aliquam dui sem, mattis vel lobortis non, euismod vitae mi. Nunc
+            rutrum erat et ligula tristique dapibus. Sed pulvinar condimentum
+            elementum. Etiam quam diam, laoreet vel enim non, fermentum
+            fringilla nibh. Donec dignissim leo vitae enim aliquet aliquet.
+          </p>
         </div>
-        <div class="col bg-primary">3 of 3</div>
+        <div class="col-7 bg-success">
+          <h1>HOME</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu
+            enim mauris. Quisque a pharetra justo, eget condimentum justo.
+            Praesent nisi est, ultrices eget nisl sit amet, tristique fringilla
+            ipsum. Morbi laoreet fringilla purus, ut ultricies nisl elementum
+            pulvinar. Cras eget enim mollis, convallis quam eu, feugiat nibh.
+            Phasellus at risus quis neque egestas feugiat. Aliquam malesuada,
+            massa non rhoncus euismod, ligula velit dignissim leo, vitae
+            facilisis ligula odio vitae sem. Quisque quis nulla non elit
+            volutpat cursus. Nullam tincidunt nunc a iaculis gravida. Praesent
+            ornare libero in vehicula malesuada. Cras a massa laoreet, gravida
+            quam id, condimentum lorem. Proin ornare purus quam, in rutrum
+            turpis sollicitudin non. Etiam non libero velit. Cras pretium, sem
+            eu tincidunt efficitur, ex eros pellentesque augue, eget porta
+            turpis libero id augue. Quisque id molestie felis. Aenean ultricies
+            pharetra rutrum. Etiam tempus augue tortor, ut rhoncus urna
+            malesuada sed. Integer aliquam molestie lacus quis lacinia. Sed
+            rutrum luctus risus ut molestie. Pellentesque finibus efficitur
+            lorem, ac rhoncus justo cursus a. Suspendisse elit ex, aliquet nec
+            viverra ut, pretium egestas nisi. Vivamus lorem augue, elementum ut
+            blandit ut, fermentum at justo. Fusce eleifend vulputate justo vitae
+            tristique. Maecenas maximus sed urna a placerat. Ut lacinia nulla
+            nec velit venenatis vehicula. Sed tempor ex id magna molestie
+            luctus. Pellentesque iaculis lectus vitae elit facilisis, vel
+            sodales ipsum mattis. Nam ex est, aliquam et congue tincidunt,
+            rutrum vulputate erat. Donec cursus cursus dolor sit amet laoreet.
+            Suspendisse eu feugiat diam. Maecenas scelerisque orci nibh, ut
+            finibus mauris ultrices vitae. Suspendisse vel auctor lectus.
+            Curabitur massa mi, aliquet et vulputate eu, consectetur vitae arcu.
+            Praesent in eros gravida, faucibus velit ut, elementum libero. Nulla
+            facilisi. Pellentesque quis viverra libero. Nullam ultrices luctus
+            lacus non interdum. Aliquam lacinia lorem eu nulla tincidunt, sit
+            amet tincidunt sem laoreet. Nulla facilisi. Duis elit diam, feugiat
+            ac ante et, molestie placerat est. Nullam tincidunt et tortor quis
+            egestas. Nunc ullamcorper mauris dui. Proin eu egestas libero.
+            Aliquam dui sem, mattis vel lobortis non, euismod vitae mi. Nunc
+            rutrum erat et ligula tristique dapibus. Sed pulvinar condimentum
+            elementum. Etiam quam diam, laoreet vel enim non, fermentum
+            fringilla nibh. Donec dignissim leo vitae enim aliquet aliquet.
+          </p>
+        </div>
+        <div class="col bg-primary">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu
+            enim mauris. Quisque a pharetra justo, eget condimentum justo.
+            Praesent nisi est, ultrices eget nisl sit amet, tristique fringilla
+            ipsum. Morbi laoreet fringilla purus, ut ultricies nisl elementum
+            pulvinar. Cras eget enim mollis, convallis quam eu, feugiat nibh.
+            Phasellus at risus quis neque egestas feugiat. Aliquam malesuada,
+            massa non rhoncus euismod, ligula velit dignissim leo, vitae
+            facilisis ligula odio vitae sem. Quisque quis nulla non elit
+            volutpat cursus. Nullam tincidunt nunc a iaculis gravida. Praesent
+            ornare libero in vehicula malesuada. Cras a massa laoreet, gravida
+            quam id, condimentum lorem. Proin ornare purus quam, in rutrum
+            turpis sollicitudin non. Etiam non libero velit. Cras pretium, sem
+            eu tincidunt efficitur, ex eros pellentesque augue, eget porta
+            turpis libero id augue. Quisque id molestie felis. Aenean ultricies
+            pharetra rutrum. Etiam tempus augue tortor, ut rhoncus urna
+            malesuada sed. Integer aliquam molestie lacus quis lacinia. Sed
+            rutrum luctus risus ut molestie. Pellentesque finibus efficitur
+            lorem, ac rhoncus justo cursus a. Suspendisse elit ex, aliquet nec
+            viverra ut, pretium egestas nisi. Vivamus lorem augue, elementum ut
+            blandit ut, fermentum at justo. Fusce eleifend vulputate justo vitae
+            tristique. Maecenas maximus sed urna a placerat. Ut lacinia nulla
+            nec velit venenatis vehicula. Sed tempor ex id magna molestie
+            luctus. Pellentesque iaculis lectus vitae elit facilisis, vel
+            sodales ipsum mattis. Nam ex est, aliquam et congue tincidunt,
+            rutrum vulputate erat. Donec cursus cursus dolor sit amet laoreet.
+            Suspendisse eu feugiat diam. Maecenas scelerisque orci nibh, ut
+            finibus mauris ultrices vitae. Suspendisse vel auctor lectus.
+            Curabitur massa mi, aliquet et vulputate eu, consectetur vitae arcu.
+            Praesent in eros gravida, faucibus velit ut, elementum libero. Nulla
+            facilisi. Pellentesque quis viverra libero. Nullam ultrices luctus
+            lacus non interdum. Aliquam lacinia lorem eu nulla tincidunt, sit
+            amet tincidunt sem laoreet. Nulla facilisi. Duis elit diam, feugiat
+            ac ante et, molestie placerat est. Nullam tincidunt et tortor quis
+            egestas. Nunc ullamcorper mauris dui. Proin eu egestas libero.
+            Aliquam dui sem, mattis vel lobortis non, euismod vitae mi. Nunc
+            rutrum erat et ligula tristique dapibus. Sed pulvinar condimentum
+            elementum. Etiam quam diam, laoreet vel enim non, fermentum
+            fringilla nibh. Donec dignissim leo vitae enim aliquet aliquet.
+          </p>
+        </div>
       </div>
     </div>
   );
