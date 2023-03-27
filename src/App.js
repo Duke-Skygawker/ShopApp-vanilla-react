a9294569fc4026df878ec3657a660b5297da3405
Duke-Skygawker <bart3kk990@gmail.com>
2023-03-27 11:26:04 +0200

    landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Outlet,
-  Link,
-} from 'react-router-dom';
-import MainNavbar from './components/Navbar/Navbar.js';
-import Home from './pages/Home';
-import Items from './pages/Items';
-import NewList from './pages/NewList';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainNavbar from "./components/Navbar/Navbar.js";
+import Home from "./pages/Home";
+import Items from "./pages/Items";
+import NewList from "./pages/NewList";
+import "bootstrap/dist/css/bootstrap.css";
 
 export default function App() {
   return (
