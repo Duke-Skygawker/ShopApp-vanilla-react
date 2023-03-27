ef1df64b6db17be524af75b41cde87d7d0cb3d28
Duke-Skygawker <82309145+Duke-Skygawker@users.noreply.github.com>
2023-03-27 20:50:33 +0200

    basic login form done

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,61 +1,57 @@
 import React from 'react';
+import './LandingPage.css';
 
 const LandingPage = () => {
   return (
     <div className="container text-center">
       <div className="row">
         <div className="col bg-primary"></div>
-        <div className="col-8 pt-5 bg-success">
+        <div className="col-8 pt-5 bg-success ">
           <h1 className="py-5">WELCOME TO SHOPPAPP</h1>
-          <form>
-            <div class="mb-3">
-              <label for="exampleInputEmail1" class="form-label">
+          <form className="form border border-3 border-primary p-4 rounded ">
+            <div className="mb-3">
+              <label for="exampleInputEmail1" className="form-label">
                 Email address
               </label>
               <input
                 type="email"
-                class="form-control"
+                className="form-control"
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
               />
-              <div id="emailHelp" class="form-text">
-                We'll never share your email with anyone else.
+              <div
+                id="emailHelp"
+                className="form-text"
+                style={{ color: '#21FF74' }}
+              >
+                Please login.
               </div>
             </div>
             <div class="mb-3">
-              <label for="exampleInputPassword1" class="form-label">
+              <label for="exampleInputPassword1" className="form-label">
                 Password
               </label>
               <input
                 type="password"
-                class="form-control"
+                className="form-control"
                 id="exampleInputPassword1"
               />
             </div>
-            <div class="mb-3 form-check">
-              <input
-                type="checkbox"
-                class="form-check-input"
-                id="exampleCheck1"
-              />
-              <label class="form-check-label" for="exampleCheck1">
-                Check me out
-              </label>
-            </div>
-            <button type="submit" class="btn btn-primary">
-              Submit
+            <button type="submit" className="btn btn-outline-warning">
+              Login
             </button>
+            <div>
+              <p className="mt-5 mb-1">
+                Don't have an account?
+                <a className="signup-link" href="#">
+                  Signup
+                </a>
+              </p>
+            </div>
           </form>
         </div>
         <div className="col bg-primary">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu
-            enim mauris. Quisque a pharetra justo, eget condimentum justo.
-            Praesent nisi est, ultrices eget nisl sit amet, tristique fringilla
-            ipsum. Morbi laoreet fringilla purus, ut ultricies nisl elementum
-            pulvinar. Cras eget enim mollis, convallis quam eu, feugiat nibh.
-            Phasellus at risus quis neque egestas feugiat.
-          </p>
+          
         </div>
       </div>
     </div>
